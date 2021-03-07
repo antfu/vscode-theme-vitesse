86739cd00bf3c3bc4e4413e0e98c54193ff002c6
Anthony Fu <anthonyfu117@hotmail.com>
2021-03-07 16:24:57 +0800

    chore: refactor to ts

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { promises as fs } from 'fs'
+import getTheme from './theme'
+
+const lightTheme = getTheme({
+  style: 'light',
+  name: 'Vitesse Light',
+})
+
+const darkTheme = getTheme({
+  style: 'dark',
+  name: 'Vitesse Dark',
+})
+
+fs.mkdir('./themes', { recursive: true })
+  .then(() => Promise.all([
+    fs.writeFile('./themes/light.json', JSON.stringify(lightTheme, null, 2)),
+    fs.writeFile('./themes/dark.json', JSON.stringify(darkTheme, null, 2)),
+  ]))
+  .catch(() => process.exit(1))
---
+++ src/primer.ts
@@ -0,0 +1,22 @@
+import { colors } from './colors'
+
+export function getColors(style) {
+  if (style === 'dark') {
+    /* The array of light to dark colors are reversed to auto-generate dark theme */
+    const darkColors: any = {}
+    Object.entries(colors).forEach(([name, val]) => {
+      if (name === 'black')
+        darkColors.white = val
+
+      else if (name === 'white')
+        darkColors.black = val
+
+      else
+        darkColors[name] = [...val].reverse()
+    })
+    return darkColors
+  }
+  else {
+    return colors
+  }
+}
